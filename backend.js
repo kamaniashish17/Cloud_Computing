fbe724f5afd90e341a1e2751ac5708d18ab9c253
Ashis Kamani <akamani@asu.edu>
2024-02-14 23:00:57 -0700

    updated files

--- backend.js
+++ backend.js
@@ -1,125 +1,62 @@
-// const express = require('express')
+const express = require("express"); // required for creating the server
+const fs = require("fs"); //streaming the csv file
+const csv = require("csv-parser"); // required for parsing the file
 
-// const app = express()
-
-// const PORT = 5000
-
-// app.get('/', (req,res)=>{
-//     console.log("Request", req)
-//     console.log("Response",res)
-// })
-
-// app.listen(PORT, "3.19.67.235", ()=>{
-//     console.log("Server running at host!!!!!")
-// })
-
-const express = require('express');
-const fs = require('fs');
-const csv = require('csv-parser');
+//initializing the instance of the server
 const app = express();
-const filePath = 'dataset/test.csv'; // Path to your CSV fil
 
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-// const ipAddress = '3.19.67.235'; // Replace with your desired IP address
-const port = 5002; // Replace with your desired port number
-const data_csv = {}
-// app.use('/', (req, res) => {
-// //   console.log("req",req)
-//   console.log("Response---->", res)
-// //   res.send('Hello World!');
-// });
+//File Path to the CSV
+const filePath = "dataset/test.csv";
 
-// function readFilenameFromRequest(filename) {
-//     // Assuming filename contains just the name of the file without path
-//     return filename;
-// }
-function readCSVFile(filePath, column1, column2, callback) {
-    console.log(filePath)
-    fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('data', (row) => {
-            // console.log("row", row.key)
-            // Assuming the CSV file has two columns: 'key' and 'value'
-            // data_csv[row.key] = row.value;
-            data_csv[row[column1]] = row[column2];
-        })
-        .on('end', () => {
-            callback(null, data_csv);
-        })
-        .on('error', (error) => {
-            callback(error, null);
-        });
-}
+//For adding middleware for handling multipart/formdata
+const multer = require("multer");
 
-readCSVFile(filePath, 'Image', 'Results', (error, data_csv) => {
-    if (error) {
-        console.error('Error reading CSV file:', error);
-    } else {
-        // console.log('Data:', data_csv);
-    }
+//For uploading the image files under the images folder
+const upload = multer({ dest: "images/" });
+
+//Setting the port number
+const port = 5002;
+
+//Storing the csv file data in an object for lookup
+const data_csv = {};
+
+const readCsvFile = (filePath, column1, column2, callback) => {
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (row) => {
+      data_csv[row[column1]] = row[column2];
+    })
+    .on("end", () => {
+      callback(null, data_csv);
+    })
+    .on("error", (error) => {
+      callback(error, null);
+    });
+};
+
+readCsvFile(filePath, "Image", "Results", (error, data_csv) => {
+  if (error) {
+    console.error("Error reading CSV file:", error);
+  } else if (data_csv) {
+    console.log("Data Extracted Successfully", Object.keys(data_csv).length);
+  }
 });
-// // Function to read datasets from CSV file
-// function readDatasetsFromCSV(csvFilePath, callback) {
-//     const datasets = {};
 
-//     fs.createReadStream(csvFilePath)
-//         .pipe(csv())
-//         .on('data', (row) => {
-//             // Assuming the CSV file has columns 'image' and 'results'
-//             datasets[row.image] = row.results;
-//             callback
-//         })
-//         .on('end', () => {
-//             callback(datasets);
-//         });
-// }
-
-function removeExtension(filename) {
-    return filename.split('.').slice(0, -1).join('.');
+const removeExtension =(filename)=> {
+  return filename.split(".").slice(0, -1).join(".");
 }
 
-// const readDataFromCSV= (filenameWithoutExtension, filePath, column1, column2, callback) =>{
-//     const data = {}
-//     fs.createReadStream(filePath)
-//         .pipe(csv())
-//         .on('data', (row) => {
-//             // Assuming column1 and column2 exist in each row
-//             data[row[column1]] = row[column2];
-//         })
-//         .on('end', () => {
-//             callback(null, data);
-//         })
-//         .on('error', (error) => {
-//             callback(error, null);
-//         });
-
-//     // console.log("Data------>", data[filenameWithoutExtension])
-
-// }
-
-
-// function findResult(inputFilename, datasets) {
-//     return datasets[inputFilename] || 'Result not found';
-// }
-
-app.use('/', upload.single('inputFile'), function (req, res) {
-    // req.file is the name of your file in the form above, here 'uploaded_file'
-    // req.body will hold the text fields, if there were any 
-    const filename = req.file.originalname;
-    const filenameWithoutExtension = removeExtension(filename);
-    // console.log("File Name", filename)
-    console.log("FileName.extension", filenameWithoutExtension)
-    if(data_csv[filenameWithoutExtension]){
-        res.send(`${filenameWithoutExtension}` + ':' + `${data_csv[filenameWithoutExtension]}`)
-    }
-//     // readDatasetsFromCSV("dataset/test.csv", (datasets) => {
-//     //     const result = findResult(filenameWithoutExtension)
-//     //     res.send({ [filenameWithoutExtension]: result });
-//     // });
-
-//     // console.log(req.file, req.body)
- });
+app.use("/", upload.single("inputFile"), function (req, res) {
+  const filename = req.file.originalname;
+  const filenameWithoutExtension = removeExtension(filename);
+  if (data_csv[filenameWithoutExtension]) {
+    res.send(
+      `${filenameWithoutExtension}` +
+        ":" +
+        `${data_csv[filenameWithoutExtension]}`
+    );
+  }
+});
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at PORT: ${port}`);
 });
